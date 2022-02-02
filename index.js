427c7f8e3774a0df71b7e283633c714420449b53
Andrey <81263467+GUN-SET@users.noreply.github.com>
2022-02-02 13:10:53 +0200

    The options have been moved to a separate file

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
 const TelegramApi = require('node-telegram-bot-api')
+const {gameOptions, againOptions} = require("./options");
 
-const token = 'YOUR_TOKEN'
+const token = '5061101652:AAEv8idu7tH6OhVLe_YkHl9_4zCFSZ9Qjms'
 
 const bot = new TelegramApi(token, {polling: true})
 
 const chats = {};
-
-const gameOptions ={
-	reply_markup:  JSON.stringify({
-		inline_keyboard: [
-			[{text: '1', callback_data: '1'},{text: '2', callback_data: '2'},{text: '3', callback_data: '3'}],
-			[{text: '4', callback_data: '4'},{text: '5', callback_data: '5'},{text: '6', callback_data: '6'}],
-			[{text: '7', callback_data: '7'},{text: '8', callback_data: '8'},{text: '9', callback_data: '9'}],
-			[{text: '0', callback_data: '0'}]
-		]
-	})
-}
-
-const againOptions ={
-	reply_markup:  JSON.stringify({
-		inline_keyboard: [
-			[{text: 'Играть еще раз', callback_data: '/again'}]
-		]
-	})
-}
 
 const startGame = async (chatId) => {
 	await bot.sendMessage(chatId, `Сейчас я загадаю цифру от 0 до 9, а ты должен её отгадать!`)
